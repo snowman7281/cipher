d8211feb73259799d6ebbceb06ebcc24f11c9094
Megan Olson and Snow Vilay <snow@>
2017-08-07 16:22:15 -0700

    add functions to js file

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var questionInput = prompt("Enter a sentence:");
+var output1 = questionInput.charAt(0).toUpperCase();
+var output2 = questionInput.charAt(questionInput.length-1).toUpperCase();
+var output =  output1.concat(output2);
+alert(output);
+
+var reverseInput = output.split("").reverse().join('');
+alert(reverseInput);
+
+var thirdFunction = questionInput.concat(reverseInput);
+alert(thirdFunction);
+
+var fourthFunction = Math.floor(questionInput.length / 2);
+var index = questionInput.charAt(fourthFunction);
+alert(index.concat(questionInput);
